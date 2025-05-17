d68cd5f5c71ac19610d0cb7d6724cf79b7fa2cf8
Leonardo <leonardosodrexx23@gmail.com>
2025-05-17 01:27:08 -0300

    Criando as Controllers

---
+++ ProjetoTop/Controllers/UsuárioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoTop.Controllers
+{
+    public class UsuárioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
