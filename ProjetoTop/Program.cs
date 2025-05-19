368fc6dd8d447060d1f0c3f27bb2b76ef54f09f0
Leonardo <leonardosodrexx23@gmail.com>
2025-05-18 21:44:37 -0300

    Configurando injeção de dependencia

--- ProjetoTop/Program.cs
+++ ProjetoTop/Program.cs
@@ -1,7 +1,11 @@
+using ProjetoTop.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<LoginRepositorio>();
+builder.Services.AddScoped<ProdutoRepositorio>();
 
 var app = builder.Build();
 
